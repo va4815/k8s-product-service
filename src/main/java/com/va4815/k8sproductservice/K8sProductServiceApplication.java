ed980f0c7a91b98fd55b5f20e2cfd454ef16a379
va4815 <va543567@gmail.com>
2026-03-06 09:46:58 +0000

    setup spring boot project

---
+++ src/main/java/com/va4815/k8sproductservice/K8sProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.va4815.k8sproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sProductServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/va4815/k8sproductservice/K8sProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.va4815.k8sproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
